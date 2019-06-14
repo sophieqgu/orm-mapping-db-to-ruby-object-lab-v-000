c741bd7d77b6fe65621af353d8e63e6c2228fcb4
Sophie Gu <sophieqgu@gmail.com>
2019-06-14 01:52:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,18 @@ class Student
     end 
   end 
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL 
+      SELECT * FROM students 
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
+  
+  def self. 
   
   def save
     sql = <<-SQL
